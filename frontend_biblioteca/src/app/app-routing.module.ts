dc98db9fe50169171384a31ad70a1bea4e19444c
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-02-28 00:23:44 -0500

    actualizar rutas

--- frontend_biblioteca/src/app/app-routing.module.ts
+++ frontend_biblioteca/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './login/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'a', component: SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend_biblioteca/src/app/login/login.module.ts
+++ frontend_biblioteca/src/app/login/login.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignInComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
