f74768424489e0e1c07f37957a876e3e089d54ed
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-02-28 15:54:39 -0500

    Actualizar api usuarios

--- frontend_biblioteca/src/app/app-routing.module.ts
+++ frontend_biblioteca/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './login/sign-in/sign-in.component';
+import { LoginModule } from './login/login.module';
 
 const routes: Routes = [
-  {path: 'a', component: SignInComponent}
+  //{ path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: '', component: SignInComponent, loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+
+
+  //{path: '',   redirectTo: '/sign-in', pathMatch: 'full' },
+  //{path: 'sign-in', component: SignInComponent}
+  
 ];
 
 @NgModule({
--- frontend_biblioteca/src/app/app.module.ts
+++ frontend_biblioteca/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorsComponent } from './errors/errors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorsComponent
   ],
   imports: [
     BrowserModule,
