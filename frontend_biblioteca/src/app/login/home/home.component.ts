71277718d8d5beeaab5a103fe4949de6891b8485
Lusho24 <lxespinosa@espe.edu.ec>
2024-02-29 10:09:00 -0500

    home

--- frontend_biblioteca/src/app/login/home/home.component.ts
+++ frontend_biblioteca/src/app/login/home/home.component.ts
@@ -7,12 +7,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  constructor(
-    private router: Router
-  ){}
-
-  signIn(user: string): void {
-    this.router.navigate([user]);
-  }
 
 }
