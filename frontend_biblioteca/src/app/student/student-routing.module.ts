4ed8f9df68f3279517187f1883d5de842178e369
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-03-05 21:57:33 -0500

    Actualizar Rutas

--- frontend_biblioteca/src/app/student/student-routing.module.ts
+++ frontend_biblioteca/src/app/student/student-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentLayoutComponent } from './student-layout/student-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:StudentLayoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
