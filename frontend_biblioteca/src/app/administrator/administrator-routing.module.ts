60ca98f664422a46c28f0eae24b975fee44a274b
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-03-06 13:29:49 -0500

    Actualizar rutas

--- frontend_biblioteca/src/app/administrator/administrator-routing.module.ts
+++ frontend_biblioteca/src/app/administrator/administrator-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdministratorLayoutComponent } from './administrator-layout/administrator-layout.component';
 import { BooksComponent } from './books/books.component';
 import { LoansComponent } from './loans/loans.component';
+import { BooksFormComponent } from './books/books-form/books-form.component';
+import { BooksWarningComponent } from './books/books-warning/books-warning.component';
 
 const routes: Routes = [
   { path: '', component:AdministratorLayoutComponent },
   { path: 'books', component: BooksComponent},
+  { path: 'books/form', component: BooksFormComponent},
+  { path: 'books/warning', component: BooksWarningComponent},
   { path: 'loans', component: LoansComponent}
 ];
 
