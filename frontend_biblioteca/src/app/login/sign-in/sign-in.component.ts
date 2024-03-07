01a199b68d6dd67876e5cc821cc7818ec7fadc91
Ricardogm2000 <rsgrijalva@espe.edu.ec>
2024-03-06 23:04:36 -0500

    Gestion de usuarios

--- frontend_biblioteca/src/app/login/sign-in/sign-in.component.ts
+++ frontend_biblioteca/src/app/login/sign-in/sign-in.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,8 +11,10 @@ import { Router } from '@angular/router';
 })
 export class SignInComponent {
 
+  mensajeError: string = '';  
+
   _hide = true;
-  
+
   private _formLogin: FormGroup = this._formBuilder.group({
     idUser: ['', Validators.required],
     password: ['', Validators.required]
@@ -20,12 +23,52 @@ export class SignInComponent {
   constructor(
     public dialogRef: MatDialogRef<SignInComponent>,
     private _formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private usersService: UsersService
   ){}
 
   signIn(user: string): void {
     this.router.navigate([user]);
     this.closeSignIn();
+  }
+
+  loadSession(): void {
+    const id = this._formLogin.value.idUser;
+    const contrasenia = this._formLogin.value.password;
+    this.usersService.autenticacion(id, contrasenia).subscribe(
+      (response) => {
+        console.log(response);
+        if (response.mensaje === 'Contraseña correcta') {
+          const rol = response.rol;
+          switch (rol) {
+            case 'estudiante':
+              this.router.navigate(['/estudiante']);
+              break;
+            case 'administrador':
+              this.router.navigate(['/administrador']);
+              break;
+            case 'superadmin':
+              this.router.navigate(['/admin']);
+              break;
+            default:
+              break;
+          }  
+          this.dialogRef.close(); 
+        } else {
+          this.mensajeError = response.mensaje;
+        }
+      },
+      (error) => {
+        console.error(error);
+        if (error.status === 401) {
+          this.mensajeError = 'Credenciales incorrectas. Inténtalo de nuevo.';
+        } else if (error.status === 404) {
+          this.mensajeError = 'Usuario no encontrado. Verifica el ID ingresado.';
+        } else {
+          this.mensajeError = 'Error al intentar iniciar sesión. Por favor, inténtalo de nuevo más tarde.';
+        }
+      }
+    );
   }
 
   closeSignIn(): void {
