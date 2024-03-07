7c985474e44ee7f65d253fb1b5b801e1859e9334
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-03-07 01:49:29 -0500

    Actualizar Rutas

--- frontend_biblioteca/src/app/admin/admin-routing.module.ts
+++ frontend_biblioteca/src/app/admin/admin-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { UsersDeleteComponent } from './users-delete/users-delete.component';
+import { UsersFormsComponent } from './users-forms/users-forms.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
-  { path: '', component:AdminLayoutComponent }
+  { path: '', component:AdminLayoutComponent },
+  { path: 'users/delete', component:UsersDeleteComponent },
+  { path: 'users/forms', component:UsersFormsComponent },
+  { path: 'users/list', component:UsersListComponent }
 ];
 
 @NgModule({
