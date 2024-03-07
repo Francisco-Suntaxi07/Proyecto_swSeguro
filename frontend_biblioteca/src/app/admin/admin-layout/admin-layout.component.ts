36f44324dfd57ea0f90770dade3585be8d6025aa
Ricardogm2000 <rsgrijalva@espe.edu.ec>
2024-03-07 02:09:21 -0500

    Actualizacion Front con datos

--- frontend_biblioteca/src/app/admin/admin-layout/admin-layout.component.ts
+++ frontend_biblioteca/src/app/admin/admin-layout/admin-layout.component.ts
@@ -1,27 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersFormsComponent } from '../users-forms/users-forms.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UsersDeleteComponent } from '../users-delete/users-delete.component';
 import { UsersListComponent } from '../users-list/users-list.component';
+import { UsersService } from 'src/app/services/users.service';
+
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
 })
-export class AdminLayoutComponent {
+export class AdminLayoutComponent implements OnInit {
+  userData: { id: string, nombre: string, apellido: string, rol: string } | null = null;
+
   constructor(
-    public dialog: MatDialog
-  ){}
+    public dialog: MatDialog,
+    private userService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.getUserDataObservable().subscribe(userData => {
+      this.userData = userData;
+    });
+  }
+
   deleteUser(): void {
     const dialogRef = this.dialog.open(UsersDeleteComponent);
     dialogRef.afterClosed().subscribe(result => {
     });
   }
+
   createUser(): void {
     const dialogRef = this.dialog.open(UsersFormsComponent);
     dialogRef.afterClosed().subscribe(result => {
     });
   }
+
   getUsers(): void {
     const dialogRef = this.dialog.open(UsersListComponent);
     dialogRef.afterClosed().subscribe(result => {
--- frontend_biblioteca/src/app/services/users.service.ts
+++ frontend_biblioteca/src/app/services/users.service.ts
@@ -1,24 +1,52 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { UsersModel } from '../models/usersModel';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class UsersService {
 
   private urlEndPoint: string = 'http://127.0.0.1:5000';
   private adminToken: string = 'secreto_admin';
+
+  private userDataSubject: BehaviorSubject<{ id: string, nombre: string, apellido: string, rol: string } | null> =
+    new BehaviorSubject<{ id: string, nombre: string, apellido: string, rol: string } | null>(null);
+
   constructor(private http: HttpClient) { }
 
-  
+  public getUserDataObservable(): Observable<{ id: string, nombre: string, apellido: string, rol: string } | null> {
+    return this.userDataSubject.asObservable();
+  }
+
+  private setUserData(userData: { id: string, nombre: string, apellido: string, rol: string }): void {
+    this.userDataSubject.next(userData);
+  }
+
   public autenticacion(id: string, contrasenia: string): Observable<any> {
     const data = { id: id, contrasenia: contrasenia };
 
-    return this.http.post<any>(`${this.urlEndPoint}/usuarios/verificar`, data);
+    return this.http.post<any>(`${this.urlEndPoint}/usuarios/verificar`, data)
+      .pipe(
+        map(response => {
+          if (response && response.id && response.nombre && response.apellido && response.rol) {
+            const userData = {
+              id: response.id,
+              nombre: response.nombre,
+              apellido: response.apellido,
+              rol: response.rol
+            };
+            this.setUserData(userData);
+          } else {
+            console.error('La respuesta del servidor no contiene la información del usuario esperada.');
+          }
+          return response;
+        })
+      );
   }
+  
   public createUser(user: UsersModel): Observable<any> {
     return this.http.post<any>(`${this.urlEndPoint}/usuarios`, user);
   }
